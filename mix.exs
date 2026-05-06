db345ae4656cdc34466c021c74776232ab14541f
Aleksei Matiushkin <am@ambment.cat>
2026-05-06 16:23:58 +0200

    :paperclip: version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Typle.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @source_url "https://github.com/Oeditus/typle"
   @homepage_url "https://github.com/Oeditus/typle"
 
